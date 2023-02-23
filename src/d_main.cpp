698d335ff71d4ad4c830d566ab445ee43aa7c815
John Stebbins <jstebbins@jetheaddev.com>
2023-02-23 07:32:58 -0700

    Allow GAMEINFO LOAD to load directories
    Addresses part of enhancement issue #2011

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -1773,6 +1773,7 @@ static FString ParseGameInfo(TArray<FString> &pwads, const char *fn, const char 
 	FScanner sc;
 	FString iwad;
 	int pos = 0;
+	bool isDir;
 
 	const char *lastSlash = strrchr (fn, '/');
 
@@ -1806,7 +1807,7 @@ static FString ParseGameInfo(TArray<FString> &pwads, const char *fn, const char 
 				{
 					checkpath = sc.String;
 				}
-				if (!FileExists(checkpath))
+				if (!DirEntryExists(checkpath, &isDir))
 				{
 					pos += D_AddFile(pwads, sc.String, true, pos, GameConfig);
 				}
