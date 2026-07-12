9e86fd42082f00684311c7b4263f6fa0ba9c743b
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2026-07-11 23:58:10 -0300

    more fixes for int size/signedness mismatches

--- src/common/engine/i_net.cpp
+++ src/common/engine/i_net.cpp
@@ -488,10 +488,11 @@ static void SendPacket(const sockaddr_in& to)
 
 	uint8_t* dataStart = &TransmitBuffer[4];
 	uLong size = MaxTransmitSize - 5u;
+	assert((NetBufferLength) <= ULONG_MAX);
 	if (NetBufferLength >= MinCompressionSize)
 	{
 		*dataStart = NetBuffer[0] | NCMD_COMPRESSED;
-		const int res = compress2(dataStart + 1, &size, NetBuffer + 1, NetBufferLength - 1u, 9);
+		const int res = compress2(dataStart + 1, &size, NetBuffer + 1, static_cast<unsigned long>(NetBufferLength - 1u), 9);
 		if (res != Z_OK)
 			I_Error("Net compression failed (zlib error %d)", res);
 
@@ -500,7 +501,7 @@ static void SendPacket(const sockaddr_in& to)
 	else
 	{
 		memcpy(dataStart, NetBuffer, NetBufferLength);
-		size = NetBufferLength;
+		size = static_cast<unsigned long>(NetBufferLength);
 	}
 
 	if (size + 4 > MaxTransmitSize)
@@ -920,7 +921,7 @@ static bool Host_CheckForConnections(void* connected)
 				memcpy(&NetBuffer[3], GameID, 8);
 				NetBufferLength = 11u;
 
-				TArrayView<uint8_t> stream = TArrayView(&NetBuffer[NetBufferLength], MAX_MSGLEN - NetBufferLength);
+				TArrayView<uint8_t> stream = TArrayView(&NetBuffer[NetBufferLength], static_cast<uint32_t>(MAX_MSGLEN - NetBufferLength));
 				Net_SetGameInfo(stream);
 				NetBufferLength += stream.Data() - &NetBuffer[NetBufferLength];
 				SendPacket(con.Address);
@@ -946,7 +947,7 @@ static bool Host_CheckForConnections(void* connected)
 							NetBufferLength += addrSize;
 						}
 
-						TArrayView<uint8_t> stream = TArrayView(&NetBuffer[NetBufferLength], MAX_MSGLEN - NetBufferLength);
+						TArrayView<uint8_t> stream = TArrayView(&NetBuffer[NetBufferLength], static_cast<uint32_t>(MAX_MSGLEN - NetBufferLength));
 						Net_SetUserInfo(i, stream);
 						NetBufferLength += stream.Data() - &NetBuffer[NetBufferLength];
 						SendPacket(con.Address);
@@ -1248,7 +1249,7 @@ static bool Guest_ContactHost(void* unused)
 				{
 					Connected[c].Status = CSTAT_READY;
 				}
-				TArrayView<uint8_t> stream = TArrayView(&NetBuffer[byte], MAX_MSGLEN - byte);
+				TArrayView<uint8_t> stream = TArrayView(&NetBuffer[byte], static_cast<unsigned>(MAX_MSGLEN - byte));
 				Net_ReadUserInfo(c, stream);
 				SetClientAck(consoleplayer, c, true);
 
@@ -1288,7 +1289,7 @@ static bool Guest_ContactHost(void* unused)
 			NetBuffer[1] = PRE_USER_INFO;
 			NetBufferLength = 2u;
 
-			TArrayView<uint8_t> stream = TArrayView(&NetBuffer[NetBufferLength], MAX_MSGLEN - NetBufferLength);
+			TArrayView<uint8_t> stream = TArrayView(&NetBuffer[NetBufferLength], static_cast<unsigned>(MAX_MSGLEN - NetBufferLength));
 			Net_SetUserInfo(consoleplayer, stream);
 			NetBufferLength += stream.Data() - &NetBuffer[NetBufferLength];
 			SendPacket(Connected[0].Address);
